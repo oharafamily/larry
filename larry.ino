c37014a74e0637224012120fd277b42d19796a20
Elliott O'Hara <ohara@csdisco.com>
2019-06-18 19:52:06 -0500

    Initial Commit

---
+++ larry.ino
@@ -0,0 +1,146 @@
+//. Motor driver shield- 2012 Copyright (c) Seeed Technology Inc.
+// 
+//  Original Author: Jimbo.we
+//  Contribution: LG
+//  
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+int pinI1=8;//define I1 interface
+int pinI2=11;//define I2 interface 
+int speedpinA=9;//enable motor A
+int pinI3=12;//define I3 interface 
+int pinI4=13;//define I4 interface 
+int speedpinB=10;//enable motor B
+const int default_speed=400;//define the spead of motor
+const int max_speed = 700;
+const int pingPin = 38;
+ 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pinI1,OUTPUT);
+  pinMode(pinI2,OUTPUT);
+  pinMode(speedpinA,OUTPUT);
+  pinMode(pinI3,OUTPUT);
+  pinMode(pinI4,OUTPUT);
+  pinMode(speedpinB,OUTPUT);
+}
+
+
+long inches_from_obstical()
+{
+  long duration, inches, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH pulse
+  // whose duration is the time (in microseconds) from the sending of the ping
+  // to the reception of its echo off of an object.
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  return inches;
+ 
+
+}
+
+long microsecondsToInches(long microseconds) {
+  // According to Parallax's datasheet for the PING))), there are 73.746
+  // microseconds per inch (i.e. sound travels at 1130 feet per second).
+  // This gives the distance travelled by the ping, outbound and return,
+  // so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+void forward(int s)
+{
+     analogWrite(speedpinA,s);//input a simulation value to set the speed
+     analogWrite(speedpinB,s);
+     digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
+     digitalWrite(pinI3,LOW);
+     digitalWrite(pinI2,LOW);//turn DC Motor A move anticlockwise
+     digitalWrite(pinI1,HIGH);
+}
+void backward(int s)//
+{
+     analogWrite(speedpinA,s);//input a simulation value to set the speed
+     analogWrite(speedpinB,s);
+     digitalWrite(pinI4,LOW);//turn DC Motor B move anticlockwise
+     digitalWrite(pinI3,HIGH);
+     digitalWrite(pinI2,HIGH);//turn DC Motor A move clockwise
+     digitalWrite(pinI1,LOW);
+}
+void left(int s)//
+{
+     analogWrite(speedpinA,s);//input a simulation value to set the speed
+     analogWrite(speedpinB,s);
+     digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
+     digitalWrite(pinI3,LOW);
+     digitalWrite(pinI2,HIGH);//turn DC Motor A move clockwise
+     digitalWrite(pinI1,LOW);
+}
+void right(int s)//
+{
+     analogWrite(speedpinA,s);//input a simulation value to set the speed
+     analogWrite(speedpinB,s);
+     digitalWrite(pinI4,LOW);//turn DC Motor B move anticlockwise
+     digitalWrite(pinI3,HIGH);
+     digitalWrite(pinI2,LOW);//turn DC Motor A move clockwise
+     digitalWrite(pinI1,HIGH);
+}
+void stop()//
+{
+     digitalWrite(speedpinA,LOW);// Unenble the pin, to stop the motor. this should be done to avid damaging the motor. 
+     digitalWrite(speedpinB,LOW);
+     delay(1000);
+ 
+}
+
+void loop()
+{
+  while(inches_from_obstical() > 5){
+    forward(200);
+    delay(100);
+  }
+  while(inches_from_obstical() < 5) {
+    for(int i = 0; i<=5; i++){
+      backward(100);
+      right(100)  ;
+    }
+  }
+  
+}
